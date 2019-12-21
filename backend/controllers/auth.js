fe45d9e383fdf071cfbe352a4869d9a4a7661c16
Noer Paanakker <noer@lightworksweb.nl>
2019-12-21 19:02:51 +0100

    added signup functionality

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,30 @@
+const User = require('./../db/models/User');
+const shortId = require('shortid');
+
 exports.postSignUp = (req, res) => {
 	const {
 		body: { name, email, password },
 	} = req;
 
-	res.send(`it works ${name}`);
+	// First check if user already exists
+	User.findOne({ email }).exec((err, user) => {
+		// If there's a user send feedback that email is already taken
+		if (user) {
+			return res.status(400).json({ error: 'Email is already taken!' });
+		} else {
+			// Else, create new user
+			let username = shortId.generate();
+			let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+			let newUser = new User({ name, email, password, profile, username });
+
+			newUser.save((err, success) => {
+				// If user didn't get save, send feedback with error
+				if (err) {
+					return res.status(422).json({ error: err });
+				}
+				return res.status(200).json({ user: success });
+			});
+		}
+	});
 };
--- backend/db/models/User.js
+++ backend/db/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const Schema = mongoose.Schema;
 
+// Define blueprint for User model
 const UserSchema = new Schema(
 	{
 		username: {
@@ -34,7 +35,7 @@ const UserSchema = new Schema(
 			type: String,
 			required: true,
 		},
-		salt: { type: String },
+		salt: String,
 		about: {
 			type: String,
 		},
@@ -50,6 +51,48 @@ const UserSchema = new Schema(
 	},
 	{ timestamps: true }
 );
+
+// This is a property that's not stored in DB
+UserSchema.virtual('password')
+	// Needs to be ES5 in order to create 'this'
+	.set(function(password) {
+		// Temp variable _password
+		this._password = password;
+
+		// Generate salt for encrypting password
+		this.salt = this.makeSalt();
+		console.log('this.salt', this.salt);
+		// Encrypt password
+		this.hashed_password = this.encryptPassword(password);
+		console.log('hashed password', this.hashed_password);
+	})
+	.get(() => this._password);
+
+// Add new methods to schema
+UserSchema.methods = {
+	authenticate: plainPassword => {
+		// Compare encrypted password with hashed password
+		return this.encryptPassword(plainPassword) === this.hashedPassword;
+	},
+	encryptPassword: function(password) {
+		// Check if there's password
+		if (!password) {
+			return '';
+		}
+
+		try {
+			return crypto
+				.createHmac('sha1', this.salt)
+				.update(password)
+				.digest('hex');
+		} catch (error) {
+			return '';
+		}
+	},
+	makeSalt: () => {
+		return Math.round(new Date().valueOf() * Math.random() + '');
+	},
+};
 
 const User = mongoose.model('User', UserSchema) || mongoose.models.User;
 
